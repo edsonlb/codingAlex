ad61b78a197fb112db3c5d25f116e7bd532127b1
Alexsander <alexsander@alexsander-linux.(none)>
2014-04-16 00:51:38 -0300

    again

--- src/codingAlex/settings.py
+++ src/codingAlex/settings.py
@@ -66,12 +66,14 @@ WSGI_APPLICATION = 'codingAlex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-##DATABASES = {
- ##   'default': {
-  ##      'ENGINE': 'django.db.backends.sqlite3',
-   ##     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    ##}
-#}
+"""
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}"""
+
 
 DATABASES = {
   'default': {
